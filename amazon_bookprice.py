d02befd6100d938f4762443fcbf8879df34fb236
gangamaha <ganga.mahadevan5@gmail.com>
2016-08-01 22:19:12 -0400

    Update amazon_bookprice.py
    updated comments

--- amazon_bookprice.py
+++ amazon_bookprice.py
@@ -3,6 +3,7 @@ import sys
 
 version = 3
 my_python_version = sys.version_info[0]  # My python version is 2.7 so my_python_version will be 2
+# get user input and import packages according to Python version
 if my_python_version < version:
     keyword = raw_input("Enter the book name you want to purchase:").strip()
     import urllib2 as py_url  # import urllib2 for Python 2.x
@@ -54,7 +55,6 @@ def get_url_details(raw_html):
 def extract_product_details(product_page):
     start_line = product_page.find("<span class=\"sims-fbt-this-item a-text-bold\">")
     with open(books_file, 'a') as output:  # output is redirected to this file.
-        # if file output.txt exist in same directory the contents will be appended
         if start_line != -1:
             start_product_name = product_page.find("This item:</span><span>",start_line+1)
             end_product_name = product_page.find("</span><span class=\"a-size-small\">", start_product_name+1)
@@ -77,7 +77,8 @@ def get_product_details(product):
         resp = py_url.urlopen(req)
         product_page = resp.read()  # raw html content for every book link
         extract_product_details(product_page)  # get name and price of book, pass the html content as parameter
-    except:
+    except Exception as e:
+        print(str(e))
         print("Cannot reach amazon.com, check your internet connection and try again later!!")
         exit(0)
 
@@ -85,10 +86,10 @@ def get_product_details(product):
 """
 Main program logic.
 The keyword is a string and search is carried in amazon.com website
-The product details and the prices from the web search is written in file output.txt
-Please change the keyword string if you want to search for another product
+The product details and the prices from the web search is written in file keyword_details.txt
+The URL's are stored in keyword_links.txt
 """
-# keyword = raw_input("Enter the book name you want to purchase:").strip()
+# if keyword is empty gracefully exit
 if keyword is '':
     print("Looks like you have not entered any book name to be searched. Exiting!!")
     exit(0)
@@ -97,28 +98,21 @@ print("Processing your request for "+keyword+". This may take some time!!")
 if ' ' in keyword:  # if space is present, replace with + for proper search
     keyword = keyword.replace(' ', '+')
 
-
-# url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+keyword
 url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+keyword+"&rh=i%3Aaps%2Ck%3A"+keyword
-# print url
 raw_html = download_link(url)  # gets the raw content of the url search page
-# print raw_html
 overall_search_results = []
 overall_search_results = get_url_details(raw_html)  # gets the url of each search result from the entire page
-# print len(overall_search_results)
 remove_dummy_links = []
 for link in overall_search_results:
-    if link.startswith("https"):
+    if link.startswith("https"):  # remove any incorrect URL's
         remove_dummy_links.append(link)
 
-# print len(remove_dummy_links)
-remove_dummy_links = list(set(remove_dummy_links))
-# print len(remove_dummy_links)
-
+remove_dummy_links = list(set(remove_dummy_links))  # remove any possible duplicate URL's
 
 links_file = (keyword+"_links.txt").replace("+", "_")
 print("Writing the search result links in "+links_file)
 with open(links_file, "a") as out:
+    # since file is opened in append mode, same search will be updated in same output file with timestamp
     out.write("\n\nRetrieved "+str(len(remove_dummy_links))+" URL's for "+keyword.replace("+"," ")+" on "+str(datetime.datetime.now())+"\n")
     out.write\
         ("\n".join(remove_dummy_links))
@@ -129,12 +123,12 @@ books_file = (keyword + "_details.txt").replace("+", "_")
 
 print("Retrieving the price details for popular "+keyword.replace("+", " ")+" in "+books_file)
 with open(books_file, 'a') as output:  # output is redirected to this file.
+    # since file is opened in append mode, same search will be updated in same output file with timestamp
     output.write("\n\nProduct prices dated: "+str(datetime.datetime.now())+"\n")
 output.close()
-# limit_search = 20
+# limit_search = 20  # if we want to limit our price search to only 20 URL's
 for link in remove_dummy_links:
     # if limit_search >= 0:
-        # print link
         get_product_details(link)  # main logic to get book name and price.
         # limit_search -= 1
 
